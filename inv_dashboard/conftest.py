353147bd746219b8a13cf63622f2bba6f1db135e
jonathanluciospolancos <jonathan.polancos@zumtobelgroup.com>
2022-09-05 18:15:13 +0800

    add new files to project

---
+++ inv_dashboard/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from inv_dashboard.users.models import User
+from inv_dashboard.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
